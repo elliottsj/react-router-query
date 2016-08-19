cd0eb80189ac0649bb0d0c74ae5335ff3ebe8e29
Spencer Elliott <me@elliottsj.com>
2016-08-19 15:21:48 -0400

    Remove dead code in withQuery

--- src/components/withQuery.js
+++ src/components/withQuery.js
@@ -2,18 +2,9 @@
 
 import Rx, { Observable } from 'rxjs/Rx';
 import compose from 'lodash/fp/compose';
-import filter from 'lodash/fp/filter';
-import fromPairs from 'lodash/fp/fromPairs';
-import map from 'lodash/fp/map';
 import mapValues from 'lodash/fp/mapValues';
-import partition from 'lodash/fp/partition';
-import pickBy from 'lodash/fp/pickBy';
-import pipe from 'lodash/fp/pipe';
-import toPairs from 'lodash/fp/toPairs';
 import { PropTypes } from 'react';
 import { getContext, mapPropsStream, withProps } from 'recompose';
-
-import isPromise from '../utils/isPromise';
 
 function resolveQueryProps(props$) {
   const rxjsProps$ = Rx.Observable.from(props$);
@@ -26,29 +17,21 @@ function resolveQueryProps(props$) {
       .flatMap(queries => Rx.Observable.pairs(queries))
       .flatMap(([name, partialQuery]: [string, CPSFunction0<FlatRoute[]>]) => {
         const query$ = Rx.Observable.bindNodeCallback(partialQuery)();
-        return query$.map(
-          result => {
-            return [name, result]
-          },
-        );
+        return query$.map(result => [name, result]);
       })
       .scan(
-        (acc: { [name: string]: FlatRoute[] }, [name, routes]: [string, FlatRoute[]]) => {
-          return {
-            ...acc,
-            [name]: routes,
-          };
-        },
+        (acc: { [name: string]: FlatRoute[] }, [name, routes]: [string, FlatRoute[]]) => ({
+          ...acc,
+          [name]: routes,
+        }),
         {}
       );
   return rxjsProps$.combineLatest(
     queryResults$,
-    (props, queryResults) => {
-      return {
-        ...props,
-        ...queryResults,
-      }
-    },
+    (props, queryResults) => ({
+      ...props,
+      ...queryResults,
+    }),
   );
 }
 
--- src/utils/match.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-import { match } from 'react-router';
-import pify from 'pify';
-
-export default pify(match, { multiArgs: true });
